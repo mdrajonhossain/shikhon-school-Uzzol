bc2c2226e5fb902b7864f2eea6806d76f2ed6692
rajonhossain <rajonhossaindhaka@gmail.com>
2022-09-25 22:35:07 +0600

    asdfsdf
    asdfasdf

--- src/component/Home.js
+++ src/component/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
 					<div className="col-md-6 mentor">
 						<div className="mentor_one">তোমাকে জেতাতে পাশে আছে উজ্জ্বল স্যার ও পলাশ স্যার সহ শতশত মেন্টর</div>
 						<div className="mentor_two">সবার জন্য মানসম্মত পড়াশোনা নিশ্চিতের মাধ্যমে শিক্ষা জীবনের প্রতিটি ধাপ জেতার জন্য রেডি করে তোলাই আমাদের লক্ষ্য</div>
-						<button type="button">আমাদের সম্পর্কে জেনে নাও ss</button>
+						<button type="button">আমাদের সম্পর্কে জেনে নাও</button>
 					</div>
 				</div>
 			</div>
